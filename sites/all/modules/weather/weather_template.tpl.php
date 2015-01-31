3c331fd70443e356ead9c083172976c17fb27411
ankaOnyschenko <annaonyschenko@gmail.com>
2015-01-31 22:20:46 +0200

    add module

---
+++ sites/all/modules/weather/weather_template.tpl.php
@@ -0,0 +1,6 @@
+<div class="weather_block">
+	<div id="meteoprog_informer_standart" data-params="none:Chercasy:300x100:white:48x50" >
+		<a href="http://www.meteoprog.ua/ru/">погода</a><br />
+		<a href="http://www.meteoprog.ua/ru/weather/Chercasy/">Погода в  Черкассах </a><br />
+	</div><script src="http://www.meteoprog.ua/ru/weather/informer/standart.js"></script>
+</div>
